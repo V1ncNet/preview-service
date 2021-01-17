a45a31df8316f60f8ea6b2f84d9f29a684dd5144
Vincent Nadoll <vincent.nadoll@googlemail.com>
2021-01-17 20:49:11 +0100

    Update to preferred context path

--- src/constants/endpoint.ts
+++ src/constants/endpoint.ts
@@ -1,4 +1,4 @@
-// Define ENDPOINT 
-export const BASE_ENDPOINT = "/api";
+// Define ENDPOINT
+export const BASE_ENDPOINT = "/api/v1";
 
 export const SERVER_STATUS_ENDPOINT = BASE_ENDPOINT + "/server-status";
