e06501233d5e770e04417d3ad927eb83d5529811
Vincent Nadoll <vincent.nadoll@googlemail.com>
2021-01-17 20:54:52 +0100

    Resolve Base64 URIs and proxy them for the viewer

---
+++ src/app/proxy/proxy.router.ts
@@ -0,0 +1,62 @@
+import { Router } from 'express';
+import { PROXY_ENDPOINT } from '../../constants/endpoint';
+import https from 'https';
+
+export const router: Router = Router();
+
+export function proxy(url: string): string {
+  const encoded = btoa(url);
+  return createProxyUrl(encoded);
+}
+
+function atob(url: string) {
+  const buffer = Buffer.from(url, 'base64');
+  return buffer.toString('utf-8');
+}
+
+function btoa(url: string): string {
+  const buffer = Buffer.from(url, 'utf-8');
+  return buffer.toString('base64');
+}
+
+function createProxyUrl(encodedUrl: string): string {
+  return `${PROXY_ENDPOINT}/${encodedUrl}`;
+}
+
+router.get(PROXY_ENDPOINT + '/:url', (req, res) => {
+  const decoded = atob(req.params.url);
+
+  forward(decoded, (data, status, headers) => {
+    res.type('blob');
+
+    headers.forEach(entry => {
+      const { 0: key, 1: value } = entry;
+      if (value) {
+        res.setHeader(key, value);
+      }
+    });
+
+    res.status(status)
+      .send(data);
+  });
+});
+
+function forward(url: string, callback: (data: any, status: number, headers: [string, string | string[] | undefined][]) => void): void {
+  const resource = new URL(url);
+
+  const chunks: any[] = [];
+  https.get(resource, res => {
+    const { headers: incommingHeaders } = res;
+    const headers = Object.entries(incommingHeaders);
+
+    res.on('data', chunk => chunks.push(chunk));
+
+    res.on('end', () => {
+      const data = Buffer.concat(chunks);
+
+      callback(data, res.statusCode as number, headers);
+    });
+  }).on('error', error => {
+    throw error;
+  }).end();
+}
--- src/constants/endpoint.ts
+++ src/constants/endpoint.ts
@@ -2,3 +2,5 @@
 export const BASE_ENDPOINT = "/api/v1";
 
 export const SERVER_STATUS_ENDPOINT = BASE_ENDPOINT + "/server-status";
+
+export const PROXY_ENDPOINT = "/p";
