be1d65e4981d65c2bbdd3680dba0c01133e65d21
Vincent Nadoll <vincent.nadoll@googlemail.com>
2021-01-20 22:21:34 +0100

    Rename function type name

--- src/utils/nCopies.ts
+++ src/utils/nCopies.ts
@@ -1,6 +1,6 @@
-type FunctionType = <T>(n: number, value: T) => T[];
+type NCopiesFunction = <T>(n: number, value: T) => T[];
 
-export const nCopies: FunctionType = <T>(n: number, o: T) => {
+export const nCopies: NCopiesFunction = <T>(n: number, o: T) => {
   const result: T[] = [];
 
   for (let i = 0; i <= n; i++) {
