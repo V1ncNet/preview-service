4bf58e2c75604c5fbfb58f681180052df8052781
Vincent Nadoll <vincent.nadoll@googlemail.com>
2021-02-20 02:13:50 +0100

    Rename function arguments

--- src/utils/base64.ts
+++ src/utils/base64.ts
@@ -1,9 +1,9 @@
-export const atob = (url: string): string => {
-  const buffer = Buffer.from(url, 'base64');
+export const atob = (base64String: string): string => {
+  const buffer = Buffer.from(base64String, 'base64');
   return buffer.toString('utf-8');
 }
 
-export const btoa = (url: string): string => {
-  const buffer = Buffer.from(url, 'utf-8');
+export const btoa = (utf8String: string): string => {
+  const buffer = Buffer.from(utf8String, 'utf-8');
   return buffer.toString('base64');
 }
