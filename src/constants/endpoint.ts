17a6f928b6c059349f404ef39859205613ba2d98
Vincent Nadoll <vincent.nadoll@googlemail.com>
2021-03-06 19:00:39 +0100

    Flatten route path depth

--- src/constants/endpoint.ts
+++ src/constants/endpoint.ts
@@ -1,7 +1,4 @@
 // Define ENDPOINT
-export const BASE_ENDPOINT = '/api/v1';
-
-export const SERVER_STATUS_ENDPOINT = BASE_ENDPOINT + '/server-status';
-
-export const PREVIEW_ENDPOINT = BASE_ENDPOINT + '/preview';
+export const SERVER_STATUS_ENDPOINT = '/server-status';
+export const PREVIEW_ENDPOINT = '/preview';
 export const PROXY_ENDPOINT = '/p';
